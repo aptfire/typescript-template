21fa0da837865ff62338f0411e74b665bb06983f
Jason Armstrong <jason@aptfire.com>
2020-04-29 08:47:23 -0400

    Added chai-as-promised; updated code to use should instead of expect.

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -1,3 +1,8 @@
 import { config } from 'dotenv'
+import * as chai from 'chai'
+import * as chaiAsPromised from 'chai-as-promised'
+
+chai.should()
+chai.use(chaiAsPromised)
 
 config({ path: '.env.test' })
