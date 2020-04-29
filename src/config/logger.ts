454e7fbc06bf6b81c702dbbb3ecffd758a26c795
Jason Armstrong <jason@aptfire.com>
2020-04-29 08:58:41 -0400

    Added winston and a baic console configuration.

---
+++ src/config/logger.ts
@@ -0,0 +1,18 @@
+import * as winston from 'winston'
+
+// TODO: Uncomment the following line to initialize file-based logging.
+// const files = new winston.transports.File({ filename: `./logs/${new Date().toJSON().replace(/T.*$/g, '')}.log` })
+const console = new winston.transports.Console({ format: winston.format.simple() })
+
+const level = process.env['LOG_LEVEL'] || 'info'
+
+const logger = winston.createLogger({
+  level,
+  transports: [console]
+  // TODO: Uncomment the following line to initialize file-based logging.
+  // transports: [files, console]
+})
+
+logger.info(`LOGGING SYSTEM INITIALIZED: ${new Date().toISOString()}`)
+
+export default logger
--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,3 +1,8 @@
-export const sayHello = (): string => 'Hi!'
+import logger from './config/logger'
+
+export const sayHello = (): string => {
+  logger.info('Saying hi!')
+  return 'Hi!'
+}
 
 export const sayHelloAsync = async (): Promise<string> => 'Hi!'
