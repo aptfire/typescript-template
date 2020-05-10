d18823a54d318801b23ab034f0cae39d4e9cad95
jscottarmstrong <47089024+jscottarmstrong@users.noreply.github.com>
2020-05-10 13:21:30 -0400

    Update bootstrap.ts

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv'
 import * as chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
-ipmort * as chaiLike from 'chai-like'
+import * as chaiLike from 'chai-like'
 
 chai.should()
 chai.use(chaiAsPromised)
