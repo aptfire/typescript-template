8edae2ae4f53a93c27b71c864a90ce78217b60df
Jason Armstrong <jason@aptfire.com>
2020-05-21 10:23:24 -0400

    Switch from substitute to chai-spies
    * Removes fluffy-spoon/subsittute in favor chai-spies
    * Adds a repository stanza to package.json to simplify the post-configuration tasks

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -2,9 +2,11 @@ import { config } from 'dotenv'
 import * as chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
 import * as chaiLike from 'chai-like'
+import * as chaiSpies from 'chai-spies'
 
 chai.should()
 chai.use(chaiAsPromised)
 chai.use(chaiLike)
+chai.use(chaiSpies)
 
 config({ path: '.env.test' })
