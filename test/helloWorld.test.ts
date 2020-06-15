008d7a456512cbc9298ae447331099ef78cdbb5d
Jason Armstrong <jason@aptfire.com>
2020-06-15 16:17:26 -0400

    Add async test for sayHello

--- test/helloWorld.test.ts
+++ test/helloWorld.test.ts
@@ -1,15 +1,12 @@
 import * as test from 'tape'
 import { sayHello, sayHelloAsync } from '../src/helloWorld'
 
-/*
-describe('helloWorld', () => {
-  it('says hello', () => sayHello().should.eq('Hi!'))
-
-  it('says hello asynchronously', async () => sayHelloAsync().should.eventually.eq('Hi!'))
-})
-*/
-
 test('helloWorld', t => {
   t.equal(sayHello(), 'Hi!')
+  t.end()
+})
+
+test('says hello asynchronously', async t => {
+  t.equal(await sayHelloAsync(), 'Hi!')
   t.end()
 })
