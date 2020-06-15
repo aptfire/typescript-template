a45714ea1af6545f391abd6e7e814e63be1c8b2d
Jason Armstrong <jason@aptfire.com>
2020-06-15 16:01:06 -0400

    Swap mocha and chai for tape

--- test/helloWorld.test.ts
+++ test/helloWorld.test.ts
@@ -1,7 +1,15 @@
+import * as test from 'tape'
 import { sayHello, sayHelloAsync } from '../src/helloWorld'
 
+/*
 describe('helloWorld', () => {
   it('says hello', () => sayHello().should.eq('Hi!'))
 
   it('says hello asynchronously', async () => sayHelloAsync().should.eventually.eq('Hi!'))
+})
+*/
+
+test('helloWorld', t => {
+  t.equal(sayHello(), 'Hi!')
+  t.end()
 })
